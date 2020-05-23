bb781144f80029a5ea1de810ec9d803a581a3495
Tomasz Michlewski <Tomasz.Michlewski@gmail.com>
2020-05-23 13:38:43 +0200

    initial commit

---
+++ android/app/src/main/kotlin/example/flutter/chopperflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package example.flutter.chopperflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:chopperflutter/ui/home.dart';
+import 'package:flutter/material.dart';
+
+class Application extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
